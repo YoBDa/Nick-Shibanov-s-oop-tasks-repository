b9390f4695ec54a388d30f451f534970d9b5b256
YoBDa <niksiban@gmail.com>
2020-09-10 19:18:22 +0300

    Initial commit for fraction calculator.

---
+++ fraction-calculator/First-lesson-app/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace First_lesson_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tbDenominator3.Text = "";
+            tbNumerator3.Text = "";
+            int Numerator1 = 0;
+            int Numerator2 = 0;
+            int Denominator1 = 0;
+            int Denominator2 = 0;
+            if (!Int32.TryParse(tbNumerator1.Text, out Numerator1))
+            {
+                MessageBox.Show("1st numerator is wrong");
+                return;
+            }
+            if (!Int32.TryParse(tbNumerator2.Text, out Numerator2))
+            {
+                MessageBox.Show("2nd numerator is wrong");
+                return;
+            }
+            if (!Int32.TryParse(tbDenominator1.Text, out Denominator1))
+            {
+                MessageBox.Show("1st denominator is wrong");
+                return;
+            }
+            if (!Int32.TryParse(tbDenominator2.Text, out Denominator2))
+            {
+                MessageBox.Show("2nd denominator is wrong");
+                return;
+            }
+            try
+            {
+                Frac f1 = new Frac(Numerator1, Denominator1);
+                Frac f2 = new Frac(Numerator2, Denominator2);
+                Frac f3 = f1 + f2;
+                tbDenominator3.Text = f3.Denominator.ToString();
+                tbNumerator3.Text = f3.Numerator.ToString();
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tbDenominator3.Text = "";
+            tbNumerator3.Text = "";
+            int Numerator1 = 0;
+            int Numerator2 = 0;
+            int Denominator1 = 0;
+            int Denominator2 = 0;
+            if (!Int32.TryParse(tbNumerator1.Text, out Numerator1))
+            {
+                MessageBox.Show("1st numerator is wrong");
+                return;
+            }
+            if (!Int32.TryParse(tbNumerator2.Text, out Numerator2))
+            {
+                MessageBox.Show("2nd numerator is wrong");
+                return;
+            }
+            if (!Int32.TryParse(tbDenominator1.Text, out Denominator1))
+            {
+                MessageBox.Show("1st denominator is wrong");
+                return;
+            }
+            if (!Int32.TryParse(tbDenominator2.Text, out Denominator2))
+            {
+                MessageBox.Show("2nd denominator is wrong");
+                return;
+            }
+            try
+            {
+                Frac f1 = new Frac(Numerator1, Denominator1);
+                Frac f2 = new Frac(Numerator2, Denominator2);
+                Frac f3 = f1 * f2;
+                tbDenominator3.Text = f3.Denominator.ToString();
+                tbNumerator3.Text = f3.Numerator.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tbDenominator3.Text = "";
+            tbNumerator3.Text = "";
+            int Numerator1 = 0;
+            int Numerator2 = 0;
+            int Denominator1 = 0;
+            int Denominator2 = 0;
+            if (!Int32.TryParse(tbNumerator1.Text, out Numerator1))
+            {
+                MessageBox.Show("1st numerator is wrong");
+                return;
+            }
+            if (!Int32.TryParse(tbNumerator2.Text, out Numerator2))
+            {
+                MessageBox.Show("2nd numerator is wrong");
+                return;
+            }
+            if (!Int32.TryParse(tbDenominator1.Text, out Denominator1))
+            {
+                MessageBox.Show("1st denominator is wrong");
+                return;
+            }
+            if (!Int32.TryParse(tbDenominator2.Text, out Denominator2))
+            {
+                MessageBox.Show("2nd denominator is wrong");
+                return;
+            }
+            try
+            {
+                Frac f1 = new Frac(Numerator1, Denominator1);
+                Frac f2 = new Frac(Numerator2, Denominator2);
+                Frac f3 = f1-f2;
+                tbDenominator3.Text = f3.Denominator.ToString();
+                tbNumerator3.Text = f3.Numerator.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            tbDenominator3.Text = "";
+            tbNumerator3.Text = "";
+            int Numerator1 = 0;
+            int Numerator2 = 0;
+            int Denominator1 = 0;
+            int Denominator2 = 0;
+            if (!Int32.TryParse(tbNumerator1.Text, out Numerator1))
+            {
+                MessageBox.Show("1st numerator is wrong");
+                return;
+            }
+            if (!Int32.TryParse(tbNumerator2.Text, out Numerator2))
+            {
+                MessageBox.Show("2nd numerator is wrong");
+                return;
+            }
+            if (!Int32.TryParse(tbDenominator1.Text, out Denominator1))
+            {
+                MessageBox.Show("1st denominator is wrong");
+                return;
+            }
+            if (!Int32.TryParse(tbDenominator2.Text, out Denominator2))
+            {
+                MessageBox.Show("2nd denominator is wrong");
+                return;
+            }
+            try
+            {
+                Frac f1 = new Frac(Numerator1, Denominator1);
+                Frac f2 = new Frac(Numerator2, Denominator2);
+                Frac f3 = f1 / f2;
+                tbDenominator3.Text = f3.Denominator.ToString();
+                tbNumerator3.Text = f3.Numerator.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ fraction-calculator/First-lesson-app/Frac.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_lesson_app
+{
+    public class Frac
+    {
+        public Exception ZeroDenominatorException = new Exception("Cannot divide by zero in fratcion.");
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+        public Frac(int Numerator, int Denominator)
+        {
+            if (Denominator == 0) throw ZeroDenominatorException;
+            this.Numerator = Numerator;
+            this.Denominator = Denominator;
+        }
+
+       
+        private static int CommonDenominator(Frac f1, Frac f2)
+        {
+            return f1.Denominator * f2.Denominator;
+        }
+        private static Frac Reduce(Frac f)
+        {
+            double Numerator = f.Numerator;
+            double Denominator = f.Denominator;
+            for (int i = Math.Abs((int)Numerator); i > 0; i--)
+            {
+                
+                if ((Numerator / i == (double)((int)Numerator / i)) && (Denominator / i == (double)((int)Denominator / i)))
+                {
+                    return new Frac((int)Numerator / i, (int)Denominator / i);
+                }
+
+            }
+            return f;
+        }
+       
+        private void ToAnotherDenominator(int NewDenominator)
+        {
+            this.Numerator = this.Numerator * (NewDenominator / this.Denominator);
+            this.Denominator = NewDenominator;
+            
+
+        }
+        public static Frac operator +(Frac f1, Frac f2)
+        {
+            int gcd = CommonDenominator(f1, f2);
+            f1.ToAnotherDenominator(gcd);
+            f2.ToAnotherDenominator(gcd);
+            
+            return Reduce(new Frac(f1.Numerator + f2.Numerator, gcd));
+        }
+        public static Frac operator *(Frac f1, Frac f2)
+            =>Reduce(new Frac(f1.Numerator * f2.Numerator, f1.Denominator * f2.Denominator));
+        public static Frac operator /(Frac f1, Frac f2)
+            => Reduce(new Frac(f1.Numerator * f2.Denominator, f1.Denominator * f2.Numerator));
+        
+        public static Frac operator -(Frac f1, Frac f2) 
+            =>Reduce((f1 + new Frac(-f2.Numerator, f2.Denominator)));
+
+    }
+}
